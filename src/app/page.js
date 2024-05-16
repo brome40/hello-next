8b42e951d2f510150090c0263cb7cfb6e4c46916
brome40 <85357500+brome40@users.noreply.github.com>
2024-05-16 00:31:45 -0400

    dynamic counter

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@ import React from 'react';
 import HitCounter from '../components/HitCounter/HitCounter';
 import Censored from '../components/Censored/Censored';
 
+export const dynamic = 'force-dynamic';
+
 function Home() {
 
   return (
